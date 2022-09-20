73552265909a97d7ac349bd310aa65dbdb8f59d2
Ruvindya <2019e117@eng.jfn.ac.lk>
2022-09-20 23:28:02 +0530

    InsertPostTable

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express =require('express')
 const app =express()
 
+const db = require("./models");
+
+db.sequelize.sync().then(() => {
+//when we start our api first itgoes to our module and go over every single tables exits in module
+//entry point of API
 app.listen(3001,() =>{
     console.log("Server running on port 3001");
-})
+});
+
+});
